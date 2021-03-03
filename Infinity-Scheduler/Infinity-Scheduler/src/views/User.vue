8a28f867262cd984724a9dc465b1fa725abc3b14
Quella-Cold <1825962863@qq.com>
2021-03-03 00:37:52 -0800

    add change password css

--- Infinity-Scheduler/Infinity-Scheduler/src/views/User.vue
+++ Infinity-Scheduler/Infinity-Scheduler/src/views/User.vue
@@ -1,5 +1,46 @@
 <template>
   <div class="body">
+      <div class="change_pass" v-show='change_pas'>
+        <div class="pass_container">
+          <label> New Password: </label>
+          <br>
+          <br>
+          <input type="text" placeholder="New Password..." />
+          <br>
+          <br>
+          <label> Comfirm New Password: </label>
+          <br>
+          <br>
+          <input type="text" placeholder="Comfirm New Password..." />
+          <br>
+          <br>
+          <br>
+          <br>
+          <button @click='change_pas = false'> Submit </button>
+          <button @click='change_pas = false' class="sec_but"> Cancel </button>
+        </div>
+      </div>
+
+      <div class="change_pass" v-show='change_email'>
+        <div class="pass_container">
+          <label> Current Password: </label>
+          <br>
+          <br>
+          <input type="text" placeholder="Current Password..." />
+          <br>
+          <br>
+          <label> New Email Address: </label>
+          <br>
+          <br>
+          <input type="text" placeholder="New Email Address..." />
+          <br>
+          <br>
+          <br>
+          <br>
+          <button @click='change_email = false'> Submit </button>
+          <button @click='change_email = false' class="sec_but"> Cancel </button>
+        </div>
+      </div>
 
       <div class="pro">
         <img id = "profile" src="user_temp.png" alt="icon" />
@@ -11,11 +52,10 @@
       <div class = "vertical_line"></div>
         <div class = "name_container">Tom</div>
         <form class = "func">
-          <div>Change Password</div>
+          <button class = "change_pass_button" @click='change_pas = true'>Change Password</button>
           <br>
-          <div>Switch Account</div>
+          <button class = "change_pass_button" @click='change_email = true'>Change Email Address</button>
         </form>
-
   </div>
 
 </template>
@@ -26,12 +66,14 @@ export default {
     name: 'User',
     data(){
         return {
+          change_pas: false,
+          change_email: false
         }
     },
     methods: {
       //local methods go here
       logout() {
-          this.$store.dispatch("logout");
+          this.$router.push({name: 'Login'});
       }
     },
     components: {
@@ -120,7 +162,56 @@ export default {
     font-family: vidaloka;
   }
 
+ .change_pass_button {
+   border: none;
+   background-color: inherit;
+   font-size: inherit;
+   font-family: inherit;
+   padding: 0;
+   cursor: pointer;
+   outline: none;
+   margin-bottom: 10px;
+ }
 
+ .change_pass {
+   position: absolute;
+   left: 25%;
+   top: 30%;
+   min-height: 40%;
+   min-width: 50%;
+   background-color: white;
+   border-radius: 10px;
+   box-shadow: 5px 5px 5px rgb(50, 50, 50);
+   padding-top: 20px;
+ }
 
+ .pass_container {
+   padding-top: 50px;
+   width: 50%;
+   margin-left: 25%;
+ }
+
+ .change_pass input {
+   width: 95%;
+   border: 2px solid black;
+   border-radius: 5px;
+   margin-bottom: 20px;
+ }
+
+ .change_pass label {
+   width: 100%;
+ }
+
+ .change_pass button {
+   width: 160px;
+   height: 40px;
+   font-size: inherit;
+   font-family: inherit;
+   float: left;
+ }
+
+ .change_pass .sec_but {
+   float: right;
+ }
 
 </style>
