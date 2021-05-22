74fcf878b22d077a0c3f6b4edfa3d423d40c5893
Zhifeng Lu <luzhi@oregonstate.edu>
2021-05-21 21:49:00 -0700

    added clock

--- Infinity-Scheduler/Infinity-Scheduler/src/views/nav_home.vue
+++ Infinity-Scheduler/Infinity-Scheduler/src/views/nav_home.vue
@@ -220,7 +220,29 @@
             }
         }, false);
     }
-    
+
+    window.onload = setInterval(function () {
+        var d = new Date();
+
+        var date = d.getDate();
+
+        var month = d.getMonth();
+        var montharr = ["Jan", "Feb", "Mar", "April", "May", "June", "July", "Aug", "Sep", "Oct", "Nov", "Dec"];
+        month = montharr[month];
+
+        var year = d.getFullYear();
+
+        var day = d.getDay();
+        var dayarr = ["Sun", "Mon", "Tues", "Wed", "Thurs", "Fri", "Sat"];
+        day = dayarr[day];
+
+        var hour = d.getHours();
+        var min = d.getMinutes();
+        var sec = d.getSeconds();
+
+        document.getElementById("date").innerHTML = month + " " + date + " " + year + " " + day;
+        document.getElementById("time").innerHTML = hour + ":" + min + ":" + sec;
+    }, 1000);
 
     //this will be time of next event
     
